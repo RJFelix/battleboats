16e506260b5e9bb84bbb52d66aa94372a67ef871
Ryan Felix <therjfelix@gmail.com>
2018-07-01 01:45:28 -0700

    Swap game into App in place of CRA default

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import GameWrapper from './component/GameWrapper';
+import Lobby from './component/Lobby';
+import BeginTurn from './component/BeginTurn';
+import Playing from './component/Playing';
+import EndGame from './component/EndGame';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <GameWrapper
+        lobby={Lobby}
+        beginTurn={BeginTurn}
+        playing={Playing}
+        endGame={EndGame}
+      />
     );
   }
 }
