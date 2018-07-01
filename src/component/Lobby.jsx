3babb7ffefbabd6c30f4f5fd23ba61fb2764c9a8
Ryan Felix <therjfelix@gmail.com>
2018-07-01 01:45:02 -0700

    Fix another derpy error

--- src/component/Lobby.jsx
+++ src/component/Lobby.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 
-const Lobby = game => (
+const Lobby = ({ game }) => {
+  return (
   <div>
     <h1>Welcome to Battleboats!</h1>
     <button onClick={
       () => game.beginGame()
     }>Start!</button>
   </div>
-)
+  );
+}
 
 export default Lobby;
