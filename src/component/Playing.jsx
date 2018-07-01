040cc265031eb7a498fd423ecd221326f732f423
Ryan Felix <therjfelix@gmail.com>
2018-07-01 02:31:52 -0700

    Add boards to Playing component, update test to work

--- src/component/Playing.jsx
+++ src/component/Playing.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Board from './Board';
 
 class Playing extends React.Component {
   constructor(props) {
@@ -10,6 +11,8 @@ class Playing extends React.Component {
     this.handleXChange = this.handleXChange.bind(this);
     this.handleYChange = this.handleYChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleOwnZoneClick = this.handleOwnZoneClick.bind(this);
+    this.handleEnemyZoneClick = this.handleEnemyZoneClick.bind(this);
   }
 
   handleXChange(e) {
@@ -26,6 +29,16 @@ class Playing extends React.Component {
     });
   }
 
+  handleOwnZoneClick() {
+    // TODO: not this.
+    alert("Please don't shoot at your own ships!");
+  }
+
+  handleEnemyZoneClick(coords) {
+    const shootAt = this.props.game.activePlayer === 1 ? 2 : 1;
+    this.props.game.shoot(shootAt, coords);
+  }
+
   handleSubmit(e) {
     e.stopPropagation();
     const shootAt = this.props.game.activePlayer === 1 ? 2 : 1;
@@ -33,9 +46,24 @@ class Playing extends React.Component {
   }
 
   render() {
+    const game = this.props.game;
+    const activePlayer = game.players.find(p => p.id === game.activePlayer);
+    const otherPlayer = game.players.find(p => p.id !== game.activePlayer);
     return (
       <div>
         <h1>Make Your Move!</h1>
+        <p>Enemy Zone</p>
+        <Board
+          player={otherPlayer}
+          revealAll={false}
+          onClick={this.handleEnemyZoneClick}
+        />
+        <p>Your Zone</p>
+        <Board
+          player={activePlayer}
+          revealAll={true}
+          onClick={this.handleOwnZoneClick}
+        />
         <p>Shoot at...</p>
         <input type='number' onChange={this.handleXChange} />
         <input type='number' onChange={this.handleYChange} />
--- src/component/Playing.test.js
+++ src/component/Playing.test.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Playing from './Playing';
+import Game from '../game/Game';
 
 describe('Playing component', () => {
   it('should pass smoke test', () => {
-    const mockGame = {
-      activePlayer: 1,
-      shoot: () => {},
-    };
+    // we do need a real game here
+    // (or, rather, mocking one is more trouble than it's worth)
+    const game = new Game();
+    game.beginGame();
+    game.play();
     const div = document.createElement('div');
-    ReactDOM.render(<Playing game={mockGame} />, div);
+    ReactDOM.render(<Playing game={game} />, div);
   })
 })
